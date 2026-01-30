d7ef71e0701fdc153d79388f5bcdacaaa979f1a7
zfabricio <fabricioguleste42@gmail.com>
2026-01-29 22:04:14 -0400

    feat: adiciona servidor http e tunnel para acesso externo

--- app.ts
+++ app.ts
@@ -1,12 +1,25 @@
 import cluster from 'node:cluster';
+import http from 'node:http';
 import { cpus } from 'node:os';
 
-const numCPUs: number = cpus().length;
-
 if (cluster.isPrimary) {
-  console.log(`[MASTER] Cluster em TS iniciado com ${numCPUs} CPUs.`);
-  for (let i = 0; i < numCPUs; i++) cluster.fork();
+  const numCPUs = cpus().length;
+  console.log(`[MESTRE]: Cluster iniciado. Criando ${numCPUs} operários.`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`[MESTRE]: Operário ${worker.process.pid} terminou. Reiniciando...`);
+    cluster.fork();
+  });
 } else {
-  console.log(`[WORKER] Nó ${process.pid} operando.`);
-  process.exit(0);
+  http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end(`Olá do Cluster! Respondido pelo PID: ${process.pid}\n`);
+    console.log(`[WORKER ${process.pid}]: Pedido recebido!`);
+  }).listen(8000);
+
+  console.log(`[WORKER]: Operário ${process.pid} online na porta 8000.`);
 }
